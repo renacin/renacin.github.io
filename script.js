fe06a00f405a0807c1bdd6538a5ead4bba1c152f
renacin <renacin.matadeen@ryerson.ca>
2020-05-18 17:30:37 -0400

    Can we have multiple maps, and what data layers can we add?

--- script.js
+++ script.js
@@ -1,50 +1,21 @@
 //
 // Name:                                            Renacin Matadeen
-// Date:                                               05/12/2020
-// Title                                            Web-Design Basics
+// Date:                                               05/18/2020
+// Title                                            MapBox Basics
 //
 // ----------------------------------------------------------------------------------------------------------------------
 
-// navigator["appCodeName"]
-// navigator["appName"]
-// navigator["appMinorVersion"]
-// navigator["cpuClass"]
-// navigator["platform"]
-// navigator["plugins"]
-// navigator["opsProfile"]
-// navigator["userProfile"]
-// navigator["systemLanguage"]
-// navigator["userLanguage"]
-// navigator["appVersion"]
-// navigator["userAgent"]
-// navigator["onLine"]
-// navigator["cookieEnabled"]
-// navigator["mimeTypes"]
+mapboxgl.accessToken = "pk.eyJ1IjoicmVuYWNpbiIsImEiOiJja2E4bjltazUwMHZnMnlqcGlxaWIwM204In0.iHbzIg1r383eOg505sRukg"
+const map = new mapboxgl.Map({
+    container: "MainMap",
+    style: "mapbox://styles/renacin/ckaciuja04m4q1is2ujckpkeh",
+    center: [-79.378048,  43.684352],
+    pitch: 0, // pitch in degrees
+    bearing: -16.20, //bearing in degrees
+    zoom: 11.11,
 
-// Create A Function To Store Everything
-function display_computerstats() {
+})
 
-    const browser_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    const browser_height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
-    var str_width = "Display Width: " + browser_width;
-    var str_height = "Display Height: " + browser_height;
-
-    document.getElementById("Display_Width").innerHTML = str_width;
-    document.getElementById("Display_Height").innerHTML = str_height;
-
-    console.log(navigator);
-
-    var OSName="Unknown OS";
-    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-    if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-
-    console.log('Your OS: '+OSName);
-
-
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-display_computerstats()
+// disable map zoom when using scroll
+map.scrollZoom.disable();
+map.pan.disable();
